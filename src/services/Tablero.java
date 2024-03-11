f956d32263cb51d1ca96f02e0ecc579557f3096a
alxrgdriguez <sralexred@gmail.com>
2024-03-11 13:57:53 +0100

    Tablero (hacerTablero)

--- src/services/Tablero.java
+++ src/services/Tablero.java
@@ -2,6 +2,7 @@ package services;
 
 import entities.Carta;
 import entities.Casilla;
+import entities.Impuesto;
 import entities.Jugador;
 
 import java.util.ArrayList;
@@ -20,7 +21,34 @@ public class Tablero {
         this.cartas = new ArrayList<>();
     }
 
-    //public void
+    /**
+     * Generar 40 casillas
+     * 1 Inicio (Casilla) --> 1
+     * 1 Carcel (Casilla) --> 31 IN JAIN hasta la casilla 11
+     * 5 Impuesto (Casilla) --> 6, 13, 18, 26, 36
+     * 5 Carta (Casilla) -->
+     * 27 Propiedades (Casilla) -->
+     */
+
+
+    public void hacerTablero(){
+
+        //  ******* 5 Impuestos (Casilla) --> 6, 13, 18, 26, 36 ******
+
+        Impuesto imp1 = new Impuesto("Impuesto Propiedad Urbana", 6, 500, 0.5);
+        Impuesto imp2 = new Impuesto("Impuesto Riqueza", 13, 1200, 4.2);
+        Impuesto imp3 = new Impuesto("Impuesto IRPF", 18, 1000,19.0);
+        Impuesto imp4 = new Impuesto("Impuesto Circulacion", 26, 750, 3.5);
+        Impuesto imp5 = new Impuesto("Impuesto Herencias",36, 1000, 2.3);
+
+        //Agregamos los impuestos en el ArrayList de casillas
+        this.addCasilla(imp1);
+        this.addCasilla(imp2);
+        this.addCasilla(imp3);
+        this.addCasilla(imp4);
+        this.addCasilla(imp5);
+
+    }
 
 
     //---- Getters ----
@@ -60,8 +88,6 @@ public class Tablero {
 
     }
 
-
-
     /**
      * Metodo para añadir una casilla al tablero
      * @param casilla nº de casilla pasa por parámetro
@@ -77,9 +103,9 @@ public class Tablero {
      * @param numeroCasilla El número de la casilla que se desea encontrar
      * @return La casilla si se encuentra, o null si no se encuentra
      */
-    public Casilla buscarCasilla(int numeroCasilla){
+    public Casilla buscarCasilla(Integer numeroCasilla){
         for (Casilla c: this.casillas) {
-            if (c.getNumero() == numeroCasilla)
+            if (c.getNumero().equals(numeroCasilla))
                 return c;
             }
         return null;
